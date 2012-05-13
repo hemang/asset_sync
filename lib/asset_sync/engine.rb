1509b425857d18b0497937d09388019ae40cf40d
hemang <whizfromberkeley@gmail.com>
2012-05-13 16:48:39 -0700

    remove deprecated engine name

--- lib/asset_sync/engine.rb
+++ lib/asset_sync/engine.rb
@@ -1,7 +1,5 @@
 class Engine < Rails::Engine
 
-  engine_name "asset_sync"
-
   initializer "asset_sync config", :group => :all do |app|
     app_initializer = Rails.root.join('config', 'initializers', 'asset_sync.rb').to_s
     app_yaml = Rails.root.join('config', 'asset_sync.yml').to_s
