95d2a9a034fc39db858e37f665327aec55cc6038
hemang <whizfromberkeley@gmail.com>
2012-05-20 14:17:57 -0700

    fix assets_prefix  config in engine

--- lib/asset_sync/engine.rb
+++ lib/asset_sync/engine.rb
@@ -31,7 +31,7 @@ class Engine < Rails::Engine
         config.gzip_compression = ENV['ASSET_SYNC_LOG'] == 'true'
         config.gzip_suffix = ENV['ASSET_SYNC_GZIP_SUFFIX'] || 'gzip'
         config.manifest = ENV['ASSET_SYNC_MANIFEST'] == 'true'
-        config.manifest = ENV['ASSET_PREFIX'] || 'assets'
+        config.assets_prefix = ENV['ASSET_PREFIX'] || 'assets'
       end
     end
 
