210184aebca1958f6253733a79e0108c5ece2eaa
hemang <whizfromberkeley@gmail.com>
2012-05-31 18:45:35 -0700

    Force reload configuration to allow multiple calls to sync from rake
    - use case: deploy to staging and prod in a single rake task by calling
    sync() twice with different configs

--- lib/asset_sync/asset_sync.rb
+++ lib/asset_sync/asset_sync.rb
@@ -6,7 +6,8 @@ module AssetSync
       @config = data
     end
 
-    def config
+    def config(force_reload=false)
+      @config = Config.new if force_reload
       @config ||= Config.new
       @config
     end
@@ -20,14 +21,17 @@ module AssetSync
       @storage ||= Storage.new(self.config)
     end
 
-    def sync
+    def sync(force_config_reload=false)
       return unless AssetSync.enabled?
 
+      #Force reload only once
       if config.fail_silently?
-        self.warn config.errors.full_messages.join(', ') unless config && config.valid?
+        self.warn config.errors.full_messages.join(', ') unless config(force_config_reload) && config.valid?
       else
-        raise Config::Invalid.new(config.errors.full_messages.join(', ')) unless config && config.valid?
+        raise Config::Invalid.new(config.errors.full_messages.join(', ')) unless config(force_config_reload) && config.valid?
       end
+      
+      #This is really a valid? check only. No reload necessary.
       self.storage.sync if config && config.valid?
     end
 
